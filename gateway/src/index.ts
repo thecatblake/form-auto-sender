9a6830362f4d11784f157792ede29e5fc363a214
ryousuke kaga <k222ryousuke@gmail.com>
2025-11-23 13:20:50 +0900

    add gateway

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,7 +1,47 @@
 import express, { Request, Response } from "express";
+import { createClient } from "redis";
+
+interface SubmitPayload {
+	url: string;
+	profile: Record<string, string>
+}
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.json());
+const redis = createClient();
+const QUEUE_KEY = process.env.QUEUE_KEY ?? "contact_submission";
+
+redis.on('error', err => console.log('Redis Client Error', err));
+
+redis
+.connect()
+.then(() => {
+	app.use(express.json());
+
+	app.get("/health", (_req: Request, res: Response) => {
+		res.status(200).json({ status: "ok" });
+	});
+
+	app.post("/submit", (req: Request, res: Response) => {
+		const { url, profile } = req.body as SubmitPayload;
+
+		if (!url || !profile) {
+			return res.status(400).json({ error: "url and profile are required"})
+		}
+
+		redis.lPush(QUEUE_KEY, req.body);
+
+		res.status(202).json({
+			message: "Jon accepted",
+			url,
+			profile
+		})
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Server listening on http://localhost:${PORT}`);
+	})
+})
+
 
