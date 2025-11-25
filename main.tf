1a982934d1fe91afc961527aec2ee56e0e1ebc1b
ryousuke kaga <k222ryousuke@gmail.com>
2025-11-25 13:22:00 +0900

    new ec2 host

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   root_block_device {
 	volume_size = 50
