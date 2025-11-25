21d476723a118b4f6dfa878e484935594f2e81e3
ryousuke kaga <k222ryousuke@gmail.com>
2025-11-25 20:02:19 +0900

    init script for ec2

--- main.tf
+++ main.tf
@@ -18,10 +18,36 @@ resource "aws_key_pair" "app_server" {
   public_key = file("~/.ssh/deploy_ansible.pub")
 }
 
+locals {
+  deploy_pubkey = chomp(file("~/.ssh/deploy_ansible.pub"))
+}
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "c5a.xlarge"
   key_name      = aws_key_pair.app_server.key_name
+
+  user_data = <<-CLOUD
+    users:
+      - name: deploy
+        shell: /bin/bash
+        groups: [sudo]
+        sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+        ssh-authorized-keys:
+          - ${local.deploy_pubkey}
+    write_files:
+      - path: /etc/ssh/sshd_config.d/10-shardening.conf
+        permissions: "0644"
+        content: |
+          PasswordAuthentication no
+          PermitRootLogin prohibit-password
+    runcmd:
+      - [ mkdir, -p, /home/deploy/.ssh ]
+      - [ chown, -R, "deploy:deploy", "/home/deploy/.ssh" ]
+      - [ chmod, "700", "/home/deploy/.ssh" ]
+      - [ chmod, "600", "/home/deploy/.ssh/authorized_keys" ]
+      - [ systemctl, restart, sshd]
+  CLOUD
 
   root_block_device {
     volume_size = 50
