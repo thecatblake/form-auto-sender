012b3d8681bcbd5163978a96251f936db69a3824
ryousuke kaga <k222ryousuke@gmail.com>
2025-11-25 11:45:31 +0900

    fix sql

--- ansible/files/setup.sql
+++ ansible/files/setup.sql
@@ -1,17 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS submit_profile {
+CREATE TABLE IF NOT EXISTS submit_profile (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT,
     body TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-}
+);
 
 CREATE TABLE IF NOT EXISTS submission_result (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     profile_id UUID,
-    target_id integer,
+    target_id INTEGER,
     contact_url TEXT,
     result TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_submission_result_profile
+      FOREIGN KEY (profile_id) REFERENCES submit_profile(id)
+);
